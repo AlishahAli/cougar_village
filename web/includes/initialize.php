6f71ec1eae4dfc4362f5fbf9666d52a91147820a
Ankit Babber <babber.ankit@gmail.com>
2017-04-12 19:32:10 -0500

    Update initialize.php

--- web/includes/initialize.php
+++ web/includes/initialize.php
@@ -16,11 +16,20 @@ string $className is the name of the class
 return void
 */
 
-
+/*
 function newAutoLoad($className)
 {
   require dirname(dirname(dirname(__FILE__))) . '/classes/' . $className . '.class.php';
 }
+*/
+
+
+require_once('classes/auth.class.php');
+require_once('classes/configure.class.php');
+require_once('classes/db.class.php');
+require_once('classes/hash.class.php');
+require_once('classes/user.class.php');
+require_once('classes/utiliti.class.php');
 
 //R!: php closing tag
 
