19e964d88bb9af38ed4b8c8632f2b32d5ce706ae
Ankit Babber <babber.ankit@gmail.com>
2017-04-13 10:05:09 -0500

    Update initialize.php

--- web/includes/initialize.php
+++ web/includes/initialize.php
@@ -6,6 +6,12 @@ initialization
 
 */
 
+require_once('classes/auth.class.php');
+require_once('classes/configure.class.php');
+require_once('classes/db.class.php');
+require_once('classes/hash.class.php');
+require_once('classes/user.class.php');
+require_once('classes/utiliti.class.php');
 
 // register autoload function
 spl_autoload_register('newAutoLoad');
@@ -16,20 +22,11 @@ string $className is the name of the class
 return void
 */
 
-/*
 function newAutoLoad($className)
 {
   require dirname(dirname(dirname(__FILE__))) . '/classes/' . $className . '.class.php';
 }
-*/
 
-
-require_once('classes/auth.class.php');
-require_once('classes/configure.class.php');
-require_once('classes/db.class.php');
-require_once('classes/hash.class.php');
-require_once('classes/user.class.php');
-require_once('classes/utiliti.class.php');
 
 //R!: php closing tag
 
