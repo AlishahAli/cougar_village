fa3c57a1233cab01e331c97abe88fc1f859f0dfe
Ankit Babber <babber.ankit@gmail.com>
2017-04-12 19:12:03 -0500

    Add files via upload

---
+++ web/includes/initialize.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+
+initialization
+
+*/
+
+
+// register autoload function
+spl_autoload_register('newAutoLoad');
+
+/*
+newAutoLoad
+string $className is the name of the class
+return void
+*/
+
+
+function newAutoLoad($className)
+{
+  require dirname(dirname(__FILE__)) . '/classes/' . $className . '.class.php';
+}
+
+//R!: php closing tag
+
+// Authorisation
+Auth::init();
+
+?>
