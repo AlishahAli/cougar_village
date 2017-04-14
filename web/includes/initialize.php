1f19db8106c2bf70aa5cdc58859ff6a9c3fd8ff1
Ankit Babber <babber.ankit@gmail.com>
2017-04-13 20:04:51 -0500

    require_once all classes without autoload

--- web/includes/initialize.php
+++ web/includes/initialize.php
@@ -7,22 +7,28 @@ initialization
 */
 
 require_once('classes/auth.class.php');
+require_once('classes/configure.class.php');
+require_once('classes/db.class.php');
+require_once('classes/hash.class.php');
+require_once('classes/user.class.php');
+require_once('classes/utiliti.class.php');
+
 
 
 // register autoload function
-spl_autoload_register('newAutoLoad');
+//spl_autoload_register('newAutoLoad');
 
 /*
 newAutoLoad
 string $className is the name of the class
 return void
 */
-
+/*
 function newAutoLoad($className)
 {
   require_once '.:/app/.heroku/php/lib/php/classes/' . $className . '.class.php';
 }
-
+*/
 
 
 
