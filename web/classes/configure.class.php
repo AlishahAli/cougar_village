47a2ffd8a5901603cfdeac9306c5d2a537839440
Ankit Babber <babber.ankit@gmail.com>
2017-04-13 20:10:17 -0500

    format update for configure

--- web/classes/configure.class.php
+++ web/classes/configure.class.php
@@ -11,12 +11,12 @@ class Config
 {
 
 //required fields for heroku
-const DB_HOST = 'ec2-184-73-236-170.compute-1.amazonaws.com',
-      DB_NAME = 'd6bj0c00g8128k',
-      DB_USER = 'wrqccthwdwoqwv',
-      DB_PASS = 'fc350d5213bb2b7c992b434e48bf6cffde0846281587bfd231a54f23f1e7428f',
-      DB_SSL  = require,
-      DB_PORT = 5432;
+    const DB_HOST = 'ec2-184-73-236-170.compute-1.amazonaws.com';
+    const DB_NAME = 'd6bj0c00g8128k';
+    const DB_USER = 'wrqccthwdwoqwv';
+    const DB_PASS = 'fc350d5213bb2b7c992b434e48bf6cffde0846281587bfd231a54f23f1e7428f';
+    const DB_SSL  = require;
+    const DB_PORT = 5432;
 
 
 }
