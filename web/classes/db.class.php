88346bc2dd08f89436fdb4eefbaff50320c0172c
Ankit Babber <babber.ankit@gmail.com>
2017-04-14 02:47:42 -0500

    update db.class.php and use in register.php

--- web/classes/db.class.php
+++ web/classes/db.class.php
@@ -11,20 +11,21 @@ prevents the need from making a new PDO connection each time
 class Database
 {
   //singleton connection obj, the underscore will indicate private
-  private static$_db;
+  private $db;
 
   //prevent creating a new obj of the class with new Database()
-  private function _construct(){}
+  //private function _construct(){}
 
   //prevent cloning the class
-  private function _clone(){}
+  //private function _clone(){}
 
   //get instance of PDO connection
   //return db PDO connection
-  public static function getInstance()
+  public function getInstance()
   {
     //R!: to change label to postgres,
     //and review if PDO instance is correctly made for heroku
+/*
     if (static::$_db === NULL)
     {
       //$dsn = 'pgsql:host=' . Config::DB_HOST . ';dbname=' . Config::DB_NAME . ';charset=utf8';
@@ -46,7 +47,18 @@ class Database
       $_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     }
+
+
     return static::$_db;
+*/
+
+      $connect_string = "host=ec2-50-19-95-47.compute-1.amazonaws.com dbname=d4j6prebdlmau9 user=qniibjvdqrucoa port=5432 sslmode=require password=5bd87c534f8455a3712867981a1a14bea89c2dd1e6b93f85ccfb8a4e41477bfa";
+
+      $db = pg_connect($connect_string);
+
+      return $db;
+
+
   }
 
 }
--- web/register.php
+++ web/register.php
@@ -8,31 +8,42 @@ register form
 
 //initialization file included here everytime
 require_once('includes/initialize.php');
-require_once('Test.php');
+//require_once('Test.php');
 
 //require the user to NOT be logged in before they can see this page
 Auth::getInstance()->requireGuest();
 
-DB_Connect::connect;
+//DB_Connect::connect;
 
 //process the submitted form
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
+/*
   //$user = User::signup($_POST);
-
   //if (empty($user->errors)) {
-
     //redirect to register_confirm.php
-    Util::redirect('/register_confirm.php');
-
-    $db = Database::getInstance();
-
-    $sql01 = "INSERT INTO website_auth_admin.user_authentication (user_name, user_email, user_password)
-            VALUES ('test', 'test', 'test')";
-
+    //Util::redirect('/register_confirm.php');
+    //$db = Database::getInstance();
+    //$sql01 = "INSERT INTO website_auth_admin.user_authentication (user_name, user_email, user_password)
+            //VALUES ('test', 'test', 'test')";
             //fname_name, empid_name, uname_name
-    $db->execute($sql01);
+    //$db->execute($sql01);
   //}
+*/
+
+echo $_POST;
+
+$db = new Database();
+$conn = $db->getInstance();
+
+$query01 = "INSERT INTO website_auth_admin.user_authentication(user_name, user_email, user_password) VALUES ('TEST3','ali@yahoo.com','alishah')";
+    $result01 = pg_query($conn,$query01);
+    //echo $result;
+
+    $query02 = "INSERT INTO website_auth_admin.employee_log(employee_display_name, employee_id, employee_user_name) VALUES ('TEST2','ali@yahoo.com','TEST3')";
+    $result02 = pg_query($conn,$query02);
+
+
+
 }
 
 include('includes/header.php');
