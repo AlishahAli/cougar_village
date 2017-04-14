dd46215e99287e9b1a18dda85bf7a077fb5af2d5
Ankit Babber <babber.ankit@gmail.com>
2017-04-13 20:07:40 -0500

    fix

--- web/classes/configure.class.php
+++ web/classes/configure.class.php
@@ -17,7 +17,7 @@ const DB_HOST = 'ec2-184-73-236-170.compute-1.amazonaws.com',
       DB_PASS = 'fc350d5213bb2b7c992b434e48bf6cffde0846281587bfd231a54f23f1e7428f',
       DB_SSL  = require,
       DB_PORT = 5432;
-*/
+
 
 }
 
