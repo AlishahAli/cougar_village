c8c691ec11f2bd580a172a1073ec4b0622b4b55e
Ankit Babber <babber.ankit@gmail.com>
2017-04-13 20:39:25 -0500

    syntax error on line 18 in configure class fixed

--- web/classes/configure.class.php
+++ web/classes/configure.class.php
@@ -15,7 +15,7 @@ class Config
     const DB_NAME = 'd6bj0c00g8128k';
     const DB_USER = 'wrqccthwdwoqwv';
     const DB_PASS = 'fc350d5213bb2b7c992b434e48bf6cffde0846281587bfd231a54f23f1e7428f';
-    const DB_SSL  = require;
+    const DB_SSL  = 'require';
     const DB_PORT = 5432;
 
 
