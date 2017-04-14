8ab26e42f79cf73f1e5dc86e64a18377040cc0e7
Ankit Babber <babber.ankit@gmail.com>
2017-04-14 04:07:23 -0500

    format

--- web/register_confirm.php
+++ web/register_confirm.php
@@ -24,7 +24,7 @@ include('includes/header.php');
   <img style="width: 300px; height: 90px; border-radius:8px, margin-left: 15px;opacity:.7;" src="https://img.clipartfest.com/d380d2184ed63d408f777340dd4f40da_roller-coaster-tracks-clip-art-cartoon-roller-coaster-clipart_3133-1495.jpeg"><br><br>
   <h3 style="position:absolute; top: 83px; left:-10px; width:100%; color: red; "> COUG</h3>
   <h3 style="position:absolute; top: 98px; left:12px; width:100%; color: black; "> VILLAGE </h3>
-  <h3 style="color: red;"> You Have Successfully Register </h3>
+  <h3 style="color: red;"> You Have Successfully Registered </h3>
   <br><br>
 
   <a id = "signbutton" href="./web/login.php" class="fakebutton">Login</a>
